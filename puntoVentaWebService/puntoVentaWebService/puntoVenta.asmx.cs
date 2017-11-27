73528973b58a9669a454591a40c060e6809fa20a
Fairbrook <32619805+Fairbrook@users.noreply.github.com>
2017-11-27 12:13:24 -0600

    CRUD Kevin

--- puntoVentaWebService/puntoVentaWebService/puntoVenta.asmx.cs
+++ puntoVentaWebService/puntoVentaWebService/puntoVenta.asmx.cs
@@ -13,7 +13,7 @@ namespace puntoVentaWebService
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    // [System.Web.Script.Services.ScriptService]
+    //[System.Web.Script.Services.ScriptService]
     public class puntoVenta : System.Web.Services.WebService
     {
         puntoventa7a1Entities DB;
@@ -21,10 +21,54 @@ namespace puntoVentaWebService
         {
             DB = new puntoventa7a1Entities();
         }
+
+
+        [WebMethod]
+        public bool createClientes(int id, string nombres, string apellidos, string direccion, string correo)
+        {
+            Clientes nuevoCliente = new Clientes {id=id, nombres=nombres, apellidos=apellidos, direccion=direccion, correo=correo };
+            DB.Clientes.Add(nuevoCliente);
+            if (DB.SaveChanges() > 0)
+            {
+                return true;
+            }
+            return false;
+        }
         [WebMethod]
         public List<Clientes> readClientes()
         {
             return DB.Clientes.ToList();
+        }
+        [WebMethod]
+        public int updateClientes(int id, string nombres, string apellidos, string direccion, string correo)
+        {
+            try
+            {
+                var cliente = DB.Clientes.Where(x=>x.id==id).FirstOrDefault();
+                cliente.nombres = nombres;
+                cliente.apellidos = apellidos;
+                cliente.direccion = direccion;
+                cliente.correo = correo;
+                return DB.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+        [WebMethod]
+        public int deleteClientes(int id)
+        {
+            try
+            {
+                var cliente = DB.Clientes.Where(x => x.id == id).FirstOrDefault();
+                DB.Clientes.Remove(cliente);
+                return DB.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
         }
        
        
