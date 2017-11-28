16ed5e11cd5380f3c04bdc9efedd4a2588ffb3b0
Eduardo Fuentes <edu05feb2010@gmail.com>
2017-11-27 21:57:27 -0600

    Interfaz Actualizada
    Hubo algunos problemas al compilar, esta versión ya tiene todos los errores corregidos.

---
+++ Interface/Interface/Historial.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Interface
+{
+    /// <summary>
+    /// Lógica de interacción para Historial.xaml
+    /// </summary>
+    public partial class Historial : Window
+    {
+        public Historial()
+        {
+            InitializeComponent();
+        }
+        private void Border_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
